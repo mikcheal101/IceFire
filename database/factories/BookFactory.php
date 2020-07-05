d57fff1ae7d877ec75a583e5a7927bce328df760
mikcheal101 <hirekaanmicheal@gmail.com>
2020-07-05 16:53:48 +0100

    hemen/Factories - BookFactory - AuthorFactory created

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Author;
+use App\Book;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Book::class, function (Faker $faker) {
+
     return [
-        //
+        'name' => $faker->name,
+        'isbn' => $faker->uuid,
+        'country' => $faker->country,
+        'number_of_pages' => 23,
+        'publisher' => $faker->name,
+        'release_date' => $faker->date(),
     ];
+});
+
+$factory->afterCreating(Book::class, function ($book, $faker) {
+    $book->authors()->save(factory(Author::class)->make());
 });
