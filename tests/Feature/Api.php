fcb439474d03ce6e685327630e67d8dec5b144ac
mikcheal101 <hirekaanmicheal@gmail.com>
2020-07-05 06:10:16 +0100

    hemen/Api.tests updated

--- tests/Feature/Api.php
+++ tests/Feature/Api.php
@@ -8,14 +8,19 @@ use Tests\TestCase;
 
 class Api extends TestCase
 {
+
+    
     /**
-     * A basic feature test example.
+     * When the endpoint:
+     * GET http://localhost:8080/api/external-books?name=:nameOfABook
+     * is requested, your application should query the Ice And Fire API 
+     * and use the data received to respond with a JSON result
      *
      * @return void
      */
-    public function testExample()
+    public function testRequirementOne()
     {
-        $response = $this->get('/');
+        $response = $this->get('');
 
         $response->assertStatus(200);
     }
