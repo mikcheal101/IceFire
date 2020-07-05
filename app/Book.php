d53839d5b8611836f9f5f9a4b926324b14b7282d
mikcheal101 <hirekaanmicheal@gmail.com>
2020-07-05 02:54:40 +0100

    hemen/book model created

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $dateFormat = 'U';
+    protected $fillable = [
+        'name',
+        'isbn',
+        'country',
+        'number_of_pages',
+        'publisher',
+        'release_date',
+    ];
+
+    /**
+     * List of authors associated with the book
+     *
+     * @return [Author]
+     */
+    public function authors()
+    {
+        return $this->hasMany(Author::class);
+    }
 }
