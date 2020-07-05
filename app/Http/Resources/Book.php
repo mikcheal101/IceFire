e7ed3345c1af6d77e4e1b4daa860daad89294e0a
mikcheal101 <hirekaanmicheal@gmail.com>
2020-07-05 06:07:18 +0100

    hemen/Book Resource - created

---
+++ app/Http/Resources/Book.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Book extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'isbn' => $this->isbn,
+            'authors' => collect($this->authors)->map(function ($author) {
+                return $author->name;
+            }),
+            'number_of_pages' => $this->number_of_pages,
+            'publisher' => $this->publisher,
+            'country' => $this->country,
+            'release_date' => Carbon::parse($this->release_date)->format('Y-m-d'),
+        ];
+    }
+}
